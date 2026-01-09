ecd8c1d578c318ee985cac6887582c2edae25c70
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-09 18:24:34 +0100

    Completato Application.java

--- lezione12_Equivalence/Application.java
+++ lezione12_Equivalence/Application.java
@@ -4,14 +4,10 @@ public class Application {
     public static void main(String[] args) {
         System.out.println("Write a comma-separated list of real numbers:");
         Scanner scanner = new Scanner(System.in);
-        BagOfReals bagOfReals1 = createBag(scanner);
+        BagOfReals bagOfReals1 = BagOfReals.createBag(scanner.nextLine());
         System.out.println("Write another comma-separated list of real numbers:");
         scanner = new Scanner(System.in);
-        BagOfReals bagOfReals2 = createBag(scanner);
+        BagOfReals bagOfReals2 = BagOfReals.createBag(scanner.nextLine());
         System.out.println(bagOfReals1.equals(bagOfReals2));
-    }
-
-    private static BagOfReals createBag(Scanner scanner) {
-
     }
 }
