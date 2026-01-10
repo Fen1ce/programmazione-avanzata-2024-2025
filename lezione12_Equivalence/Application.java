3d1269ede103e69ffece54ca6858d1dc3ed40bd3
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-10 16:08:14 +0100

    Aggiornato Application.java per verificare toString()

--- lezione12_Equivalence/Application.java
+++ lezione12_Equivalence/Application.java
@@ -5,9 +5,15 @@ public class Application {
         System.out.println("Write a comma-separated list of real numbers:");
         Scanner scanner = new Scanner(System.in);
         BagOfReals bagOfReals1 = BagOfReals.createBag(scanner.nextLine());
+        System.out.println("Your comma-separated list of real numbers is: " + bagOfReals1); //chiamata a toString() automatica data da operando +
         System.out.println("Write another comma-separated list of real numbers:");
         scanner = new Scanner(System.in);
         BagOfReals bagOfReals2 = BagOfReals.createBag(scanner.nextLine());
-        System.out.println(bagOfReals1.equals(bagOfReals2));
+        System.out.println("Your other comma-separated list of real numbers is: " + bagOfReals2); //chiamata a toString() automatica data da operando +
+        if (bagOfReals1.equals(bagOfReals2)) {
+            System.out.println("The lists are the same");
+        }  else {
+            System.out.println("The lists are not the same");
+        }
     }
 }
