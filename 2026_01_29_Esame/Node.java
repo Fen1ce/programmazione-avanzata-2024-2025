389cb1be36dd02d03e803a0639ae1e2203426277
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-29 12:42:11 +0100

    Creata classe Node.java; Creati i metodi Node.Node(), Node.addChild, Node.getValue() Node.getChild

---
+++ 2026_01_29_Esame/Node.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Un nodo contiene nodi dello stesso tipo.
+ * È questo che crea l’albero.
+ * Ogni figlio è:
+ * - radice di un sottoalbero
+ * - con la stessa struttura del padre
+ * @param <T>
+ */
+
+class Node<T> {
+    private T value;
+    private List<Node<T>> children;
+
+    Node(T value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
+    void addChild(Node<T> child) {
+        children.add(child);
+    }
+
+    /*
+    Node<T> addChild(T value) {
+        Node<T> child = new Node<>(value);
+        children.add(child);
+        return child;
+    }
+     */
+
+    T getValue() {
+        return value;
+    }
+    Node<T> getChild(int index) {
+        return children.get(index);
+    }
+}
