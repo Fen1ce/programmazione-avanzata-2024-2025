b7d852836b4a374dd3196909ce91328edeee108d
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-29 12:39:49 +0100

    Aggiornata classe Tree.java; Creati i metodi Tree.Tree() e Tree.getRoot()

--- 2026_01_29_Esame/Tree.java
+++ 2026_01_29_Esame/Tree.java
@@ -1,4 +1,17 @@
-public class Tree {
-    public List<T> arrayList = new ArraList;
-    public Tree(List arra)
+/**
+ * L’albero è solo un riferimento alla radice
+ * Tutto il resto si raggiunge da lì.
+ * @param <T>
+ */
+
+class Tree<T> {
+    private Node<T> root;
+
+    Tree(T rootValue) {
+        root = new Node<>(rootValue);
+    }
+
+    public Node<T> getRoot() {
+        return root;
+    }
 }
