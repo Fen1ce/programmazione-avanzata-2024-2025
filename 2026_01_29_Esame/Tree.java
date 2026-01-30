7c61800688aaf12de8a4ca60d0422b34de614a70
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-30 16:35:25 +0100

    Aggiunto javadoc

--- 2026_01_29_Esame/Tree.java
+++ 2026_01_29_Esame/Tree.java
@@ -1,16 +1,24 @@
 /**
- * L’albero è solo un riferimento alla radice
- * Tutto il resto si raggiunge da lì.
- * @param <T>
+ * Una classe per modellare la radice di albero.
+ * Contiene solo un riferimento al nodo che funge da radice.
  */
-
 class Tree<T> {
     private Node<T> root;
 
+    /**
+     * Costruisce un nuovo albero a partire dal nodo {@code rootvalue} che funge da radice.
+     *
+     * @param rootValue Riferimento al nodo che funge da radice dell'albero.
+     */
     Tree(T rootValue) {
         root = new Node<>(rootValue);
     }
 
+    /**
+     * Restituisce il riferimento al nodo che funge da radice dell'albero.
+     *
+     * @return Nodo radice dell'albero.
+     */
     public Node<T> getRoot() {
         return root;
     }
