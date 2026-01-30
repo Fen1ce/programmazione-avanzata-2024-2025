4a036d39c0eee67c044b31cbde1e5be5b22e7930
Fenice <187117889+Fen1ce@users.noreply.github.com>
2026-01-30 16:28:51 +0100

    Aggiunto javadoc

--- 2026_01_29_Esame/Node.java
+++ 2026_01_29_Esame/Node.java
@@ -2,27 +2,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
+ * Una classe per modellare un nodo di un albero contenente un certo valore {@code <T>} a piacere e i riferimenti ai nodi figli.
  * Un nodo contiene nodi dello stesso tipo.
- * È questo che crea l’albero.
- * Ogni figlio è:
- * - radice di un sottoalbero
- * - con la stessa struttura del padre
- * @param <T>
  */
-
 class Node<T> {
     private T value;
     private List<Node<T>> children;
 
+    /**
+     * Costruisce un nuovo nodo con il {@code value} fornito come valore.
+     *
+     * @param value Il valore del nodo. Può essere un tipo primitivo oppure un oggetto.
+     */
     Node(T value) { //perché il costruttore di Node<T> ha la maiuscola?
         this.value = value;
         this.children = new ArrayList<>();
     }
 
+    /**
+     * Aggiunge il nodo figlio {@code child} al nodo padre sul quale il metodo è stato invocato.
+     *
+     * @param child Nuovo nodo figlio. Il tipo del valore di questo nodo dovrebbe essere il medesimo di quello del nodo padre.
+     */
     void addChild(Node<T> child) {
         children.add(child);
     }
-
     /*
     Node<T> addChild(T value) {
         Node<T> child = new Node<>(value);
@@ -31,9 +35,21 @@ class Node<T> {
     }
      */
 
+    /**
+     * Restituisce il valore del nodo sul quale il metodo è stato invocato.
+     *
+     * @return Valore del nodo.
+     */
     T getValue() {
         return value;
     }
+
+    /**
+     * Restituisce l'i-esimo nodo figlio del nodo padre sul quale il metodo è stato invocato.
+     *
+     * @param index Indice dell'i-esimo nodo figlio.
+     * @return I-esimo nodo figlio.
+     */
     Node<T> getChild(int index) {
         return children.get(index);
     }
